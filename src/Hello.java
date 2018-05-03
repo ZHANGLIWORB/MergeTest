f2dfa49c91d779942a9d888aa8c17e2d2798bda4
ZHANGLIWORB <693025970@qq.com>
2018-05-03 20:15:18 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args){
+
+        String branchName = "master";
+        System.out.println("This branch is first commit:"+branchName);
+    }
+
+}
