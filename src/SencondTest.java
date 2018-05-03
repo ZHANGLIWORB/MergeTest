b9ba87f6371000268afe269d51aacd9ced5e30a4
ZHANGLIWORB <693025970@qq.com>
2018-05-03 21:10:32 +0800

    second commit

---
+++ src/SencondTest.java
@@ -0,0 +1,10 @@
+public class SencondTest {
+
+    private String name;
+    private Integer age;
+
+    public SencondTest(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
